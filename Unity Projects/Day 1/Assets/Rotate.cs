0098900db76c0d7888b7502bbe357d9ea1b4e790
Student <student@ent-v314-003.local>
2017-09-05 16:01:09 -0400

    hw1 attempt 569

---
+++ Unity Projects/Day 1/Assets/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	//public means it will show up i the inspector
+
+	//Vector3 is the variable type for 3D objects
+
+	//myRotation is the variable type ive picked for 
+	//roatiaon 
+
+	public Vector3 myRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (myRotation * Time.deltaTime);
+	}
+}
