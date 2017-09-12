275aa212bb5601dc49d69ae5daa42c672da187e6
Maci320 <student@ent-v314-003.local>
2017-09-12 15:44:01 -0400

    iteration_interaction

---
+++ Unity Projects/Day 1/Assets/Scripts/InteractionCube.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionCube : MonoBehaviour {
+
+	public Renderer rend;
+	//public Vector3 myRotation;
+
+	public GameObject go; 
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+
+	
+
+	for (int i = 0; i < 6; i++) {
+		Vector3 myVec = new Vector3 (i - 5, 2, 0);
+
+		Instantiate (go, myVec, Quaternion.identity);
+	   }
+	}
+	void OnMouseEnter(){
+		rend.material.color = Color.red;
+	}
+
+	void OnMouseExit(){
+		rend.material.color = Color.white;
+	}
+		
+	void OnMouseOver () {
+		//scale objects over three axiss
+		transform.localScale += new Vector3 (.001f, .01f, .02f);
+	}
+		void Update(){
+		//transform.Rotate ((0, 2, 2) * Time.deltaTime);	
+	}
+  
+}
---
+++ Unity Projects/Day 1/Assets/Scripts/Iteration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Iteration : MonoBehaviour {
+
+	public GameObject go;
+
+	// Use this for initialization
+	void Start () {
+		//for loop
+		//has four components
+		//ist
+		//2nd
+		//
+		for (int i = 0; i < 10; i++) {
+			Vector3 myVec = new Vector3 (i - 5, 2, 0);
+
+			Instantiate (go, myVec, Quaternion.identity);
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
