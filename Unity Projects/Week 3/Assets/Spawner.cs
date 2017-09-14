fefc0ab986f5f88a79ed39dc36580da34ef74d22
Maci320 <student@ent-v314-003.local>
2017-09-14 15:43:13 -0400

    week 3 unity review stuff

---
+++ Unity Projects/Week 3/Assets/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject go;
+
+	public float StartAfter = 0.5f;
+	public float SpawnFrequency = .3f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnThing", StartAfter, SpawnFrequency);
+	}
+	
+	// Update is called once per frame
+	void SpawnThing () {
+		Instantiate (go, transform.position, Quaternion.identity); 
+	}
+}
