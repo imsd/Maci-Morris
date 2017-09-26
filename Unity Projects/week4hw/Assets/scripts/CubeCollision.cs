cf47ffa12fb2c66842bcd56479cf3466baa5a477
Maci320 <student@ent-v314-003.local>
2017-09-26 15:43:59 -0400

    user interaction hw week 4 and mouse stuff

---
+++ Unity Projects/week4hw/Assets/scripts/CubeCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCollision : MonoBehaviour {
+
+	public float colourChangeDelay = 0.5f;
+	float currentDelay = 0f;
+	bool colourChangeCollision = false;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("Contact was made!");
+		colourChangeCollision = true;
+		currentDelay = Time.time + colourChangeDelay;
+	}
+	void checkColourChange()
+	{        
+		if(colourChangeCollision)
+		{
+			transform.GetComponent<Renderer>().material.color = Color.yellow;
+			if(Time.time > currentDelay)
+			{
+				transform.GetComponent<Renderer>().material.color = Color.white;
+				colourChangeCollision = false;
+			}
+		}
+	}
+
+	void Update()
+	{
+		checkColourChange();
+	}
+}
---
+++ Unity Projects/week4hw/Assets/scripts/mouseStuff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseStuff : MonoBehaviour {
+	public float forcePower = 500f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	// called when mouse hovering over object
+	//void OnMouseOver() {
+		// rotate the object
+		//transform.Rotate( new Vector3 (0, 90, 0) * Time.deltaTime);
+	//}
+    void OnMouseDown() {
+		Vector3 forceDirection = Vector3.forward * forcePower;
+		GetComponent<Rigidbody> ().AddForce (forceDirection);
+      
+}
+}
