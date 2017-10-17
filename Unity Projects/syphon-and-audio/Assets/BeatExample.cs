e719112c99ae348017d726cdfe310b2a9f71f691
Maci320 <student@ent-v314-003.local>
2017-10-17 15:48:01 -0400

    syphon and audio projection mapping

---
+++ Unity Projects/syphon-and-audio/Assets/BeatExample.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatExample : MonoBehaviour
+{
+
+	// the renderer for this cube
+	public Renderer rend;
+//	public float xposition;
+//	public float yposition;
+//	public float zposition;
+	public float movementspeed = 0.5f;
+//	public GameObject go;
+
+
+	void Start ()
+	{
+
+		// at start, get the Renderer component of this object
+		// and set it to rend
+		rend = GetComponent<Renderer> ();
+
+		//Select the instance of AudioProcessor and pass a reference
+		//to this object
+		AudioProcessor processor = FindObjectOfType<AudioProcessor> ();
+		processor.onBeat.AddListener (onOnbeatDetected);
+		processor.onSpectrum.AddListener (onSpectrum);
+	}
+
+	//this event will be called every time a beat is detected.
+	//Change the threshold parameter in the inspector
+	//to adjust the sensitivity
+	void update() {
+
+		//Vector3 myvec = new Vector3(xposition,yposition,zposition);
+
+	}
+	void onOnbeatDetected ()
+	{
+		
+		rend.material.color = Random.ColorHSV();
+		transform.Translate(movementspeed,0,0);
+	}
+
+	//This event will be called every frame while music is playing
+	void onSpectrum (float[] spectrum)
+	{
+		//The spectrum is logarithmically averaged
+		//to 12 bands
+
+		for (int i = 0; i < spectrum.Length; ++i) {
+			Vector3 start = new Vector3 (i, 0, 0);
+			Vector3 end = new Vector3 (i, spectrum [i], 0);
+			Debug.DrawLine (start, end);
+		}
+	}
+}
+
