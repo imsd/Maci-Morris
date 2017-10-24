77c26a728314b5df90722aae908b0897d25cf721
Maci320 <student@ent-v314-003.local>
2017-10-24 18:31:36 -0400

    unity project 2

---
+++ Unity Projects/Unity Project 2/New Unity Project/Assets/scripts/videoplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+
+public class videoplay : MonoBehaviour {
+
+	// add a .mov into your project and add it to the object,
+	// then attach this script and modify
+
+	public VideoPlayer vidp;
+
+	void Start() {
+		vidp.Play ();	// just call this whenever you need
+	}
+}
