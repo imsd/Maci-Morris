776c36637a20cbbfa2c2d369e505dcb0ef61eb12
Maci320 <student@ent-v314-003.local>
2017-11-02 17:41:08 -0400

    week 10 and some unity project 2 fixes

--- Unity Project 2/readme.md
+++ Unity Project 2/readme.md
@@ -1,3 +1,5 @@
 ![alt text](https://github.com/imsd/Maci-Morris/blob/master/Unity%20Project%202/Screen%20Shot%202017-10-31%20at%203.41.40%20PM.png)
 
+
+Group members: Maci, Donovan, Jason, Deanna
 Our project is a interactive city display. Our aim was to create a city like scene where users would be able to control a few of the elements. There are city buildings with billboards attached, a moving cube of advertisments and falling spheres. The Billboards display a video of adventure time. The rotating cube displays videos from various animes as ads. They can be user controlled by using the arrow keys and numbers 1 and 2. The spheres come in a blue and green and when the a and space keys are presses the spheres are released; they fill up the the "street". There are 4 cameras used in this scene. 
---
+++ Unity Projects/week 10 madlibs/Assets/arrayStringExample1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrayStringExample1 : MonoBehaviour {
+
+
+
+	public string[] names;
+	public string[] places;
+	public string[] adverbs; 
+	public string[] nouns; 
+	public string[] verbs;
+	public string[] names2; 
+	public string[] adjective; 
+
+	void Start () {
+	// loop thru all the names
+	//        for (int i = 0; i < names.Length; i++) {
+	//            Debug.Log (names [i]);
+	//        }
+
+	// draw a random name from the array
+	//        int rando = Random.Range(0, names.Length);
+	//        Debug.Log (names [rando]);
+
+	int randNames = Random.Range(0, names.Length);
+	int randPlace = Random.Range(0, places.Length);
+	int randAd = Random.Range(0, adverbs.Length);
+	int randNoun = Random.Range(0, nouns.Length);
+	int randVerbs = Random.Range(0, verbs.Length);
+	int randNames2 = Random.Range(0, names2.Length); 
+	int randAdj = Random.Range(0, adjective.Length);
+
+	string line1 = "One day, " + names[randNames] + " went to the " +
+		            places[randPlace] + "." + 
+		           " While there " + names2[randNames2] + " " +  
+		            adverbs[randAd] + " " +
+		            verbs[randVerbs] + " " + 
+		            nouns[randNoun] + "." + 	
+		            "After freaking out " + names[randNames] +
+		            " yelled. Bro are you " + names[randNames];
+
+
+
+
+
+
+
+		Debug.Log (line1); 
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
