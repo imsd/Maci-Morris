19232282df3ac4b4ea988a9da479b071f12fe60a
Maci320 <student@ent-v314-003.local>
2017-11-14 15:54:54 -0500

    weather week 12 iot

---
+++ Week11/Weather_Internet of Things/Assets/Weather/Snow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour {
+
+	public GameObject SnowDrop;				// what object to drop
+	public float SnowDropFrequency = 0.5f;	// how often is the drop period
+	public int DropMultiplier = 1;	        // how many drops per drop period
+	public float DropRadius = 5f;			// 
+	private float LastSnowDrop;				// when was the last rain drop
+
+	void Update () {
+
+	// is the current time greater than the last time we dropped rain,
+	// plus the rain drop frequency?
+	if (Time.time > LastSnowDrop + SnowDropFrequency) {
+
+	// drop multiplier here allows us to drop multiple rain drops at once
+	for (int i = 0; i < DropMultiplier; i++) {
+
+	// where to drop it
+	Vector3 dropLocation = transform.position + (Random.insideUnitSphere * DropRadius);
+
+	// make a rain drop
+	GameObject NewDrop = Instantiate (SnowDrop, dropLocation, Quaternion.identity);
+
+	// destroy the rain drop after 1 second
+	Destroy (NewDrop, 1f);
+
+	}
+	// record last time dropped so we can do this all again
+	LastSnowDrop = Time.time;
+	}
+
+	}
+}
