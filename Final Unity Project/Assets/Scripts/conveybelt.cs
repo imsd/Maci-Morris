ca5938c50a1372a8b57e7056e2d06e398450192a
Maci320 <student@ent-v314-003.local>
2017-12-05 15:56:34 -0500

    progress,stick to belt

--- Final Unity Project/Assets/Scripts/conveybelt.cs
+++ Final Unity Project/Assets/Scripts/conveybelt.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class conveybelt : MonoBehaviour {
+	void Start(){
 
-	// Use this for initialization
-	void Start () {
-		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+    
+	}
+	void OnCollisionEnter(Collision c) {
+		c.transform.parent = transform;
 	}
 }
